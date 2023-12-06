dd607be554e0d94cdeba4295c51a96c75b56061a
Subhransu <subhranshumajhi2@gmail.com>
2023-12-06 19:32:36 +0530

    add CORS

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/horoscope/:sunsign", async(req, res)=>{
-    const {sunsign} = req.params;
-    try{
-        const response = await axios.get(`http://sandipbgt.com/theastrologer/api/horoscope/${sunsign}/today`);
-        const data = response.data;
-        res.status(200).json(data);
-    }catch(err){
-        console.error(err);
-        res.status(500).json({error: "Internal Server Error"});
-    }
-})
+app.use(cors());
+
+app.get("/horoscope/:sunsign", async (req, res) => {
+  const { sunsign } = req.params;
+  try {
+    const response = await axios.get(
+      `http://sandipbgt.com/theastrologer/api/horoscope/${sunsign}/today`
+    );
+    const data = response.data;
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 const port = 5000;
 app.listen(port, () => {
